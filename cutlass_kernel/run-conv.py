571635e4e5d8247d7cfd8410d55f01bd8e71d699
Khoa Ho <khoaho@my.unt.edu>
2022-05-31 05:26:15 -0500

    1st commit

---
+++ cutlass_kernel/run-conv.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import os
+
+os.system("./bench_conv.bin --benchmark --iteration 1000")
---
+++ cutlass_kernel/run-gemm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+
+B = 64
+
+N_K_list = [
+    128,
+    256,
+    384,
+    512,
+    640,
+    768,
+    896,
+    1024
+]
+
+for N_K in N_K_list:
+    os.system("./bench_gemm.bin {} {} {}".format(B, N_K, N_K))
